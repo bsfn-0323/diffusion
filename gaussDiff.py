60ef0c86bb6f64fb0b9baad181fd7c97d48d675b
bsfn-0323 <stefano.bae@uniroma1.it>
2024-02-23 19:18:33 +0100

    modified:   gaussDiff.ipynb 	modified:   gaussDiff.py 	modified:   gaussDiff_constraint.ipynb 	modified:   gaussDiff_constraint_analisi.ipynb 	modified:   gaussDiff_minimization.ipynb 	modified:   todo.txt

--- gaussDiff.py
+++ gaussDiff.py
@@ -9,14 +9,14 @@ def load_data(L,temp,MCS):
 def gaussScore(x0,temp,nSteps,dt,device = "cpu"):
     x0 = torch.from_numpy(x0).to(torch.float32).to(device)
     N = x0.shape[1]
-    W = torch.Tensor(nSteps+1,N,N)
+    W = torch.Tensor(nSteps+1,N,N).to(device)
     #b = torch.Tensor(nSteps+1,N)
-    cov = torch.cov(x0.T)
-    m0 = torch.mean(x0,dim=1)
+    cov = torch.cov(x0.T).to(device)
+    m0 = torch.mean(x0,dim=1).to(device)
     for i in range(nSteps+1):
-        exp_t = torch.exp(torch.Tensor([-2*i*dt]))
+        exp_t = torch.exp(torch.Tensor([-2*i*dt])).to(device)
         exp_tsq = exp_t*exp_t
-        W[i] = torch.linalg.inv(temp*(1-exp_tsq)*torch.eye(N) + exp_tsq*cov)
+        W[i] = torch.linalg.inv(temp*(1-exp_tsq)*torch.eye(N).to(device) + exp_tsq*cov)
         #b[i] = -exp_t* torch.mm(W[i],m0)
     return W
 
@@ -26,12 +26,12 @@ def backward(xT,W,temp,nSteps,dt,full_traj = False,device = "cpu"):
     nbatches = 50
     batch_size = int(P/nbatches)
     if(full_traj):
-        x_recon = torch.Tensor(P,nSteps,N)
-        x_recon[:,-1,:] = xT
+        x_recon = torch.Tensor(P,nSteps,N).to(device)
+        x_recon[:,-1,:] = xT.to(device)
     else:
-        x_recon = xT
+        x_recon = xT.to(device)
     for tt in range(nSteps)[::-1]:
-        std = np.sqrt(2*temp*dt)*torch.randn_like(xT)
+        std = np.sqrt(2*temp*dt)*torch.randn_like(xT).to(device)
 
         for n in range(nbatches):
             if(full_traj):
@@ -42,7 +42,7 @@ def backward(xT,W,temp,nSteps,dt,full_traj = False,device = "cpu"):
                 x_recon[n*batch_size:(n+1)*batch_size] = x_recon[n*batch_size:(n+1)*batch_size]*(1+dt)+ 2*temp*score*dt + std[n*batch_size:(n+1)*batch_size]
             del score
     
-    return x_recon
+    return x_recon.to("cpu")
 
 def Dkl(x0,x_recon):
     eps = 1e-05
@@ -51,12 +51,13 @@ def Dkl(x0,x_recon):
     return np.sum(c1*np.log((c1+eps)/(c2+eps)))
 
 L = 14
+
 N = L**2
 P = 100000
 MCS = 200000
 nSteps = 300
 dt = 0.02
-temp = 8
+temp = 2
 slices = np.linspace(1,nSteps,9,dtype = np.int32) -1
 Ts = np.linspace(2.27,3.22,20)
 Dkls = np.array([])
@@ -64,11 +65,11 @@ idx = np.random.choice(range(MCS),P, replace = False)
 os.system(f"mkdir data_N{N}_T{temp:.3f}_P{P}")
 for i,T in enumerate(Ts):
     x0 = load_data(L,T,MCS)
-    W = gaussScore(x0[idx],temp,nSteps,dt)
+    W = gaussScore(x0[idx],temp,nSteps,dt,"cuda")
     xT = np.sqrt(temp)*torch.randn((P,N))
-    x_recon = backward(xT,W,temp,nSteps,dt,True).numpy()
-    np.save(f"data_N{N}_T{temp:.3f}_P{P}/recon_temp{T:.3f}_nSteps{nSteps}_dt{dt:.2f}",x_recon[:,slices,:])
-    Dkls = np.append(Dkls,Dkl(x0[idx],x_recon[:,0,:]))
+    x_recon = backward(xT,W,temp,nSteps,dt,False,"cuda").numpy()
+    np.save(f"data_N{N}_T{temp:.3f}_P{P}/recon_temp{T:.3f}_nSteps{nSteps}_dt{dt:.2f}",x_recon)
+    Dkls = np.append(Dkls,Dkl(x0[idx],x_recon))
     del x_recon
     #print(f"-----Done {i}-----")
 
